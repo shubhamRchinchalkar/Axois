5a1bee73f18c9806a056e847497cf6f8cb2799fd
Shubham Chinchalkar <150879788+shubhamRchinchalkar@users.noreply.github.com>
2024-03-05 16:51:17 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import UserList from './UserList'; // Import the UserList component
+
+const App = () => {
+  return (
+    <div className="App">
+      <UserList /> {/* Render the UserList component */}
+    </div>
+  );
+};
+
+export default App;
